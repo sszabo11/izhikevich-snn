589d6a087d9dab7bd0815e7234a68e9f10d6a417
sszabo11 <sszabo1082@gmail.com>
2026-05-10 22:26:13 +1000

    stdp

--- src/izhikevich.rs
+++ src/izhikevich.rs
@@ -24,6 +24,8 @@ pub struct Izhikevich {
 
     pub spikes: Array2<u32>,
 
+    pub spike_history: Vec<Vec<f32>>,
+
     n_neurons: usize,
     n_conns: usize,
     threshold: f32,
@@ -34,8 +36,8 @@ pub struct Izhikevich {
 
     a_plus: f32,
     a_minus: f32,
-    tau_pre: f32,
-    tau_post: f32,
+    tau_plus: f32,
+    tau_minus: f32,
 }
 
 impl Izhikevich {
@@ -43,6 +45,8 @@ impl Izhikevich {
         n_neurons: usize,
         n_conns: usize,
         threshold: f32,
+        tau_plus: f32,
+        tau_minus: f32,
         a: f32,
         b: f32,
         c: f32,
@@ -55,6 +59,11 @@ impl Izhikevich {
             b,
             c,
             d,
+            tau_plus,
+            tau_minus,
+            a_plus: 0.01,
+            a_minus: 0.02,
+            spike_history: vec![],
             threshold,
             n_conns,
             membranes: Array1::zeros(n_neurons),
@@ -71,17 +80,43 @@ impl Izhikevich {
         }
     }
 
-    fn learn(&self, pre_ms: f32, post_ms: f32) -> f32 {
-        let d_syn = post_ms - pre_ms;
+    fn apply_stdp(&mut self, post_i: usize, t_post: f32) {
+        let conns = self.conns.row(post_i);
 
-        if d_syn > 0.0 {
-            self.a_plus * (E * (d_syn / self.tau_pre))
-        } else {
-            self.a_minus * (E * (d_syn / self.tau_pre))
+        for (arr_i, &conn) in conns.iter().enumerate() {
+            let recent_spikes = &self.spike_history[conn as usize];
+
+            for t_pre in recent_spikes.iter() {
+                let d_t = -t_post - t_pre;
+
+                let mut dw = 0.0;
+
+                if d_t > 0.0 && d_t < self.tau_plus {
+                    // LTP
+                    dw = self.a_plus * (E * (-d_t / self.tau_plus).exp());
+                } else {
+                    // LTD
+                    dw = -self.a_minus * (E * (d_t / self.tau_minus).exp());
+                }
+
+                if dw != 0.0 {
+                    self.weights[[post_i, arr_i]] += dw;
+                }
+            }
         }
     }
 
-    pub fn run(&mut self, T: f32) -> ModelResponse {
+    //fn stdp(&self, pre_ms: f32, post_ms: f32) -> f32 {
+    //    let d_syn = post_ms - pre_ms;
+
+    //    if d_syn > 0.0 {
+    //        self.a_plus * (E * (d_syn / self.tau_pre))
+    //    } else {
+    //        self.a_minus * (E * (d_syn / self.tau_pre))
+    //    }
+    //}
+
+    pub fn run(&mut self, T: f32, input: Array2<f32>) -> ModelResponse {
         let dt: f32 = 0.1;
         let steps = (T / dt) as usize;
 
@@ -98,17 +133,30 @@ impl Izhikevich {
         let mut rng = rand::rng();
         for t in 0..steps {
             for i in 0..self.n_neurons {
-                let mut I = rng.random::<f32>() * 20.0;
+                let I = input[[t, i]] + I_baseline;
+
+                //let mut I = rng.random::<f32>() * 20.0;
                 let t_ms = t as f32 * dt;
 
-                if t_ms >= t_start && t_ms <= t_end {
-                    I += I_baseline;
-                }
+                //if t_ms >= t_start && t_ms <= t_end {
+                //    I += I_baseline;
+                //}
 
                 if self.membranes[i] > self.threshold {
+                    println!("aovcve");
                     self.membranes[i] = self.c; // Reset
                     self.u[i] += self.d;
                     self.spikes[[t, i]] = 1;
+
+                    self.spike_history[i].push(t_ms);
+
+                    //self.learn(i);
+                    while !self.spike_history[i].is_empty()
+                        && t_ms - self.spike_history[i][0] > 50.0
+                    {
+                        self.spike_history[i].remove(0);
+                    }
+                    //self.apply_stdp(i, t_ms);
                 }
 
                 let v = self.membranes[i];
@@ -120,8 +168,6 @@ impl Izhikevich {
                 v_values[[t, i]] = v;
                 I_values[[t, i]] = I;
                 t_values[[t, i]] = t_ms;
-
-                self.learn(pre_ms, post_ms)
             }
         }
         ModelResponse {
@@ -131,5 +177,142 @@ impl Izhikevich {
             t_values,
             threshold: self.threshold,
         }
+    }
+
+    pub fn from_builder(builder: IzhikevichBuilder) -> Self {
+        Self {
+            n_neurons: builder.n_neurons,
+            n_conns: builder.n_conns,
+            threshold: builder.threshold,
+            a: builder.a,
+            b: builder.b,
+            c: builder.c,
+            d: builder.d,
+            spike_history: vec![vec![0.0; 250]; builder.n_neurons],
+            spikes: Array2::zeros((builder.T, builder.n_neurons)),
+
+            // STDP
+            a_plus: builder.a_plus,
+            a_minus: builder.a_minus,
+            tau_plus: builder.tau_plus,
+            tau_minus: builder.tau_minus,
+
+            // Existing fields
+            membranes: Array1::zeros(builder.n_neurons),
+            u: Array1::zeros(builder.n_neurons),
+            weights: Array2::random(
+                (builder.n_neurons, builder.n_conns),
+                Uniform::new(-0.5, 0.5).unwrap(),
+            ),
+            conns: Array2::random(
+                (builder.n_neurons, builder.n_conns),
+                Uniform::new(0, builder.n_neurons as u32).unwrap(),
+            ),
+        }
+    }
+}
+
+pub struct IzhikevichBuilder {
+    n_neurons: usize,
+    n_conns: usize,
+    threshold: f32,
+    a: f32, // Time scale of the recovery variable. Smaller -> slower recovery
+    b: f32, // Sensitivty of the recovery variable. Larger -> stronger coupling
+    c: f32, // Reset voltage of membrane
+    d: f32, // Increment of the recovery variable after a spike.
+
+    a_plus: f32,
+    a_minus: f32,
+    tau_plus: f32,
+    tau_minus: f32,
+
+    T: usize,
+}
+
+impl Default for IzhikevichBuilder {
+    fn default() -> Self {
+        Self {
+            n_neurons: 1,
+            n_conns: 1,
+            threshold: 40.0,
+            a: 0.01,
+            b: 0.2,
+            T: 100,
+            c: -65.0,
+            d: 8.0,
+            a_plus: 0.01,
+            a_minus: 0.02,
+            tau_plus: 20.0,
+            tau_minus: 20.0,
+        }
+    }
+}
+
+impl IzhikevichBuilder {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn n_neurons(mut self, n_neurons: usize) -> Self {
+        self.n_neurons = n_neurons;
+        self
+    }
+    pub fn n_conns(mut self, n_conns: usize) -> Self {
+        self.n_conns = n_conns;
+        self
+    }
+
+    pub fn threshold(mut self, threshold: f32) -> Self {
+        self.threshold = threshold;
+        self
+    }
+
+    pub fn a(mut self, a: f32) -> Self {
+        self.a = a;
+        self
+    }
+
+    pub fn b(mut self, b: f32) -> Self {
+        self.b = b;
+        self
+    }
+
+    pub fn c(mut self, c: f32) -> Self {
+        self.c = c;
+        self
+    }
+
+    pub fn d(mut self, d: f32) -> Self {
+        self.d = d;
+        self
+    }
+
+    pub fn T(mut self, T: usize) -> Self {
+        self.T = T;
+        self
+    }
+
+    pub fn a_plus(mut self, a_plus: f32) -> Self {
+        self.a_plus = a_plus;
+        self
+    }
+
+    pub fn a_minus(mut self, a_minus: f32) -> Self {
+        self.a_minus = a_minus;
+        self
+    }
+
+    pub fn tau_plus(mut self, tau_plus: f32) -> Self {
+        self.tau_plus = tau_plus;
+        self
+    }
+
+    pub fn tau_minus(mut self, tau_minus: f32) -> Self {
+        self.tau_minus = tau_minus;
+        self
+    }
+
+    pub fn build(self) -> Izhikevich {
+        Izhikevich::from_builder(self)
     }
 }
